caf51b7a579ade99f4bf33fb58a38a503eb26939
Katarzyna Pi <38461666+katarzynapi@users.noreply.github.com>
2018-06-13 20:40:01 +0200

    Add files via upload

---
+++ inc/menu.php
@@ -0,0 +1,42 @@
+<!--<H4>MENU - wybierz jedną z opcji</H4>
+	<H5><ul>
+		<li><a href="<?php echo $PlikGlowny; ?>?mn=1"><?php echo $Naglowki[1][1]; ?></a></li>
+		<li><a href="00.php?mn=2"><?php echo $Naglowki[2][1]; ?></a></li>
+		<li><a href="00.php?mn=3"><?php echo $Naglowki[3][1]; ?></a></li>
+		<li><a href="00.php?mn=4"><?php echo $Naglowki[4][1]; ?></a></li>
+		<li><a href="00.php?mn=5"><?php echo $Naglowki[5][1]; ?></a></li>
+	</ul>
+	</H5>
+-->
+<div class = "container">
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a class="navbar-brand" href="">Twoja apteczka</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarColor01">
+    <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="<?php echo $PlikGlowny; ?>">Strona główna</a>
+        </li>
+      <li class="nav-item">
+        <a class="nav-link" href="<?php echo $PlikGlowny; ?>?mn=1"><?php echo $Naglowki[1][1]; ?></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="00.php?mn=2"><?php echo $Naglowki[2][1]; ?></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="00.php?mn=3"><?php echo $Naglowki[3][1]; ?></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="00.php?mn=4"><?php echo $Naglowki[4][1]; ?></a>
+      </li>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="00.php?mn=5"><?php echo $Naglowki[5][1]; ?></a>
+      </li>
+    </ul>
+  </div>
+</nav>
+</div>
